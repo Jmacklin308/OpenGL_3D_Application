583711e67189044c954ded70946769fff1073dfc
Jmacklin308 <jmacklin308@gmail.com>
2022-08-16 22:40:03 -0500

    Gots me a really red dorito

--- OpenGLCourseApp/Main.cpp
+++ OpenGLCourseApp/Main.cpp
@@ -1,8 +1,127 @@
 #include <stdio.h>
+#include <string.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+// vertex shader
+static const char* vShader = "                    \n\
+#version 330							          \n\
+												  \n\
+layout (location = 0) in vec3 pos;				  \n\
+void main()										  \n\
+{												  \n\
+    gl_Position = vec4(0.9 * pos.x, 0.4 * pos.y, 0.4 * pos.z, 1.0); \n\
+}";
+
+static const char* fShader = "                    \n\
+#version 330							          \n\
+												  \n\
+out vec4 color;                  				  \n\
+void main()										  \n\
+{												  \n\
+    color = vec4(1.0,0.0,0.0,1.0); \n\
+}";
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+	GLuint theShader = glCreateShader(shaderType);
+	const GLchar* theCode[1];
+	theCode[0] = shaderCode;
+
+	GLint codeLength[1];
+	codeLength[0] = strlen(shaderCode);
+
+	glShaderSource(theShader,1, theCode, codeLength);
+	glCompileShader(theShader);
+
+
+	GLint result = 0;
+	GLchar eLog[1024] = {0};
+
+	//create executables on GPU
+	glLinkProgram(shader);
+
+	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+	if(!result)
+	{
+		glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+		printf("ERROR compiling the %d shader: '%s'\n",shaderType, eLog);
+		return;
+	}
+
+	glAttachShader(theProgram, theShader);
+	
+}
+
+
+void CompileShaders()
+{
+	shader = glCreateProgram();
+	if(!shader)
+	{
+		printf("ERROR Creating shader program");
+		return;
+	}
+
+	AddShader(shader, vShader, GL_VERTEX_SHADER);
+	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+	
+	GLint result = 0;
+	GLchar eLog[1024] = {0};
+
+	//create executables on GPU
+	glLinkProgram(shader);
+
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+	if(!result)
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("ERROR Linking program: '%s'\n", eLog);
+		return;
+	}
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+	if(!result)
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("ERROR Validating program: '%s'\n", eLog);
+		return;
+	}
+	
+	
+}
+
+
+void CreateTriangle()
+{
+	//create Vertex array object (VAO)
+	GLfloat vertices[] = {
+		-1.0f,-1.0f,0.0f,
+		1.0f,-1.0f,0.0f,
+		0.0f,1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1,&VAO);
+	glBindVertexArray(VAO);
+
+		//create buffer object
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,0,0);
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER,0);
+
+	
+	glBindVertexArray(0);
+
+}
 
 int main()
 {
@@ -57,7 +176,10 @@ int main()
 	//setup viewport size
 	glViewport(0,0,bufferWidth, bufferHeight);
 
+	CreateTriangle();
+	CompileShaders();
 
+	
 
 	//create main loop
 	while (!glfwWindowShouldClose(window))
@@ -69,6 +191,18 @@ int main()
 		glClearColor(0.0f,0.4f,0.3f,1);
 		glClear(GL_COLOR_BUFFER_BIT); // clear all the colors. this can be used to clear various other things
 
+		glUseProgram(shader);
+
+			glBindVertexArray(VAO);
+
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+			glBindVertexArray(0);
+
+		
+		glUseProgram(0);
+
+		
 		//swap to the new buffer (double buffer)... we work on the next buffer before we swap to that one.
 		glfwSwapBuffers(window);
 		
